557df1179f9de7b2c2d78b4199d50ea0daa9beae
pr1ynsu <poddarpriyanshukr@gmail.com>
2025-05-21 09:33:30 +0530

    work

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+// src/data/projects.js
+const projects = [
+  {
+    title: "Portfolio Website",
+    description: "Built using React and Tailwind, showcasing my work with smooth transitions and responsive design.",
+    liveLink: "https://priyanshu-s-portfolio.vercel.app/",
+    githubLink: "https://github.com/pr1ynsu/Priyanshu-s-Portfolio",
+  },
+  {
+    title: "Forum App",
+    description: "An anonymous forum with real-time chat and aesthetic UI. Built with React and Node.js.",
+    liveLink: "https://anonymous-chat-authentication.vercel.app/",
+    githubLink: "https://github.com/pr1ynsu/AnonymousChat-Authentication",
+  },
+  {
+    title: "gift",
+    description: "Built using React and Tailwind, showcasing my work with smooth transitions and responsive design.",
+    liveLink: "https://gift-seven-lake.vercel.app/",
+    githubLink: "https://github.com/pr1ynsu/Smart-Grocery-Advisor",
+  },
+  {
+    title: "Smart Grocey",
+    description: "An anonymous forum with real-time chat and aesthetic UI. Built with React and Node.js.",
+    liveLink: "https://smart-grocery-advisor.vercel.app/",
+    githubLink: "https://github.com/pr1ynsu/Smart-Grocery-Advisor",
+  },
+  
+];
+
+export default projects;
